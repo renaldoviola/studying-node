5488aaff39b648eba322ef254528702d9f9767d8
Renaldo Viola <renaldo@Rocketlab.com.br>
2020-07-12 23:32:35 -0300

    adjustments

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,8 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(cors);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
